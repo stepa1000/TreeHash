e4ecb5ec8803035e8155abc192cf8b4b69751db0
stepa1000 <stepa100023@gmail.com>
2024-09-21 09:05:28 +0600

    runable.
    так как веса от -1 до 1, то приходиться добавлять к последниму слою столько нейронов сколько значений на выходном слое.
    При это добавлениям к первым слоям скорее увеличивают работу, чем количество качеств и значений.
    Востонавлений нету. Можно ещё посмотреть на среднии нейронные сети.

--- src/Other/Utils.hs
+++ src/Other/Utils.hs
@@ -48,7 +48,7 @@ metric llpn1 llpn2 =
 getRandomElementList :: [a] -> IO (Maybe a)
 getRandomElementList la = do
 	let l = P.length la
-	i <- randomRIO (0,l + 1)
+	i <- randomRIO (0,l)
 	return $ la P.!? i
 
 getRELs :: Int -> [a] -> IO [a]
